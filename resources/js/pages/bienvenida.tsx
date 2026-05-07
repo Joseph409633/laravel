42384f4c55e4f5e196b3b27318e391f7a276285c
Jose Antonio <j.alcocer.mainez@iescristobaldemonroy.es>
2026-05-07 11:32:22 +0200

    Añadiendo algunos endpoints en web.php

---
+++ resources/js/pages/bienvenida.tsx
@@ -0,0 +1,12 @@
+// resources/js/Pages/bienvenida.tsx
+import React from 'react';
+
+// Definimos una "Interface" para decir qué datos recibe el componente
+
+export default function Bienvenida() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Bienvenido, usuario.</h1>
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,14 @@ Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
+Route::inertia('/bienvenida', 'bienvenida');
+
 Route::get('/test', function () {
     return 'Este es un endpoint de prueba';
+});
+
+Route::get('/saludo/{usuario}', function($usuario){
+    return 'Bienvenido de nuevo, ' . $usuario;
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
